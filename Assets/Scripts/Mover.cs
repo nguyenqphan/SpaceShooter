c2f763826687b3fdc68a4da7c75c4f1b7576d8a0
Nguyen Phan <nqphan212@gmail.com>
2015-12-14 09:42:02 -0500

    Creat a bolt prefab which was made out of a quad and material. Write Mover script to make the bolt moves

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed; //speed of the bolt
+
+	void Awake(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Start(){
+		rb.velocity = transform.forward*speed;
+		//rb.velocity = new Vector3(0,0,1);
+	}
+}
