15584b66323ff4551843ee8380cb49292f18dae5
Nguyen Phan <nqphan212@gmail.com>
2015-12-14 18:16:23 -0500

    Creat a Asteroid for players to shoot.

---
+++ Assets/RandomRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble = 5;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+
+	}
+}
