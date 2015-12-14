887e8c01f04127df149d8674a04b1e6e80de879f
Nguyen Phan <nqphan212@gmail.com>
2015-12-14 18:33:46 -0500

    Add explosion effects to the ship and asteroid before destroying them

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,12 +3,24 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;             //refer to asteroid explosion prefab    
+	public GameObject playerExplosion;       //refer to ship explosion prefab
+
 	void OnTriggerEnter(Collider other){
 
 		//Debug.Log (other.name);
+
+		//Condition to not destroy the boundary
 		if (other.tag == "Boundary") {
 			return;
 		}
+
+		//instantiate explosion effects for the ship and asteriods before destroying them.
+		Instantiate (explosion, transform.position, transform.rotation);    
+
+		if(other.tag == "Player")
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
