cd061ec3de9f053d2dcc62d72e65d79c0db76da9
Nguyen Phan <nqphan212@gmail.com>
2015-12-17 11:55:10 -0500

    Add UI, Asteroids, enemies

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -3,18 +3,40 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject hazard; //refer to asteroids
+	public GameObject[] hazards; //refer to asteroids
 	public Vector3 spawnValues;
 	public int hazardCount;
 	public float spawWait;
 	public float startWait;
 	public float waveWait;
 
+	//UI
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
 	void Start()
 	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateSocre ();
 		StartCoroutine(SpawnWaves ());
 	}
 
+	void Update(){
+		if (restart) {
+			if(Input.GetKeyDown(KeyCode.R)){
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+	}
 
 
 	IEnumerator SpawnWaves()
@@ -26,13 +48,38 @@ public class GameController : MonoBehaviour {
 			for (int i = 0; i < hazardCount; i++) {
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+				Instantiate (hazards[Random.Range(0,hazards.Length)], spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawWait);
 			}
 
 			yield return new WaitForSeconds (waveWait);
 
+			if(gameOver){
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+
 		}
 	}
+
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateSocre ();
+
+	}
+
+	void UpdateSocre()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver(){
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+
+
 
 }
--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,22 +6,68 @@ public class DestroyByContact : MonoBehaviour {
 	public GameObject explosion;             //refer to asteroid explosion prefab    
 	public GameObject playerExplosion;       //refer to ship explosion prefab
 
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start()
+	{
+		GameObject gameConllerObject = GameObject.FindWithTag ("GameController");
+		if (gameConllerObject != null) {
+			gameController = gameConllerObject.GetComponent<GameController>();
+		}
+		if(gameController == null){
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+
 	void OnTriggerEnter(Collider other){
 
 		//Debug.Log (other.name);
 
 		//Condition to not destroy the boundary
-		if (other.tag == "Boundary") {
+		/*
+		if (other.tag == "Boundary" || other.tag == "Enemy") {
+			return;
+		}
+		*/
+
+		//Debug.Log (other.GetComponent);
+
+
+		Debug.Log(gameObject);
+
+		if(other.CompareTag("Boundary")){
+			//Debug.Log("I was here");
+//			Debug.Log(other.GetComponent);
 			return;
 		}
 
-		//instantiate explosion effects for the ship and asteriods before destroying them.
-		Instantiate (explosion, transform.position, transform.rotation);    
-		Debug.Log (explosion);
 
-		if(other.tag == "Player")
+		if(other.tag == "Enemy"){
+			Debug.Log("Find an enemy ");
+		//	Debug.Log(gameObject);
+			return;
+
+		}
+
+		//Debug.Log ("I'd better not be here");
+
+		//instantiate explosion effects for the ship and asteriods before destroying them.\
+
+		Debug.Log ("It was about to explode");
+
+		if (explosion != null) {
+			Instantiate (explosion, transform.position, transform.rotation);    
+			Debug.Log (explosion + " was down");
+		}
+		if (other.tag == "Player") {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+		}
 
+		gameController.AddScore (scoreValue);
+
+		//destroy all objects related to the collision
 		Destroy (other.gameObject);
 		Destroy (gameObject);
 	}
